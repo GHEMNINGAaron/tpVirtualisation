090c2d92f76d9e6203d06fc87c531d179a3ae872
GHEMNING Aaron <aaron.ghemning@institutsaintjean.com>
2024-05-03 13:39:59 +0100

    Version 1.1

--- FrontEnd/src/app/forget-password/forget-password.component.ts
+++ FrontEnd/src/app/forget-password/forget-password.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthServiceService } from '../../services/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
 export class ForgetPasswordComponent {
 
+  constructor(private authService: AuthServiceService, private router: Router) {}
+
+  utilisateur : any = {
+    mail: null,
+    password : null
+  }
+
+  modifiedPassword : boolean =true
+  isPassword : boolean = true
+  resetPassword(){
+    this.isPassword = Object.values(this.utilisateur).every(valeur => !!valeur);
+
+    if(this.isPassword) {
+      this.authService.resetPassword(this.utilisateur).subscribe((error) => {
+        this.modifiedPassword = false
+      }, (data) => {
+        this.router.navigate(['/login_page'])
+      })
+    }
+
+  }
 }
